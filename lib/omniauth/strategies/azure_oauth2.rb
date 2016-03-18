0a4d55967c0f3dceb7e384c7b8424e9a9182a741
Marc Lennox <marc.lennox@gmail.com>
2016-03-17 23:08:56 -0400

    Updated to the v2.0 OAuth flow

--- lib/omniauth/strategies/azure_oauth2.rb
+++ lib/omniauth/strategies/azure_oauth2.rb
@@ -1,63 +1,31 @@
 require 'omniauth/strategies/oauth2'
-require 'jwt'
 
 module OmniAuth
   module Strategies
     class AzureOauth2 < OmniAuth::Strategies::OAuth2
-      BASE_AZURE_URL = 'https://login.microsoftonline.com'
-
       option :name, 'azure_oauth2'
 
-      option :tenant_provider, nil
+      option :authorize_options, [:scope]
 
-      # AD resource identifier
-      option :resource, '00000002-0000-0000-c000-000000000000'
+      option :client_options,
+             site: 'https://login.microsoftonline.com',
+             authorize_url: '/common/oauth2/v2.0/authorize',
+             token_url: '/common/oauth2/v2.0/token'
 
-      # tenant_provider must return client_id, client_secret and optionally tenant_id
-      args [:tenant_provider]
-
-      def client
-        if options.tenant_provider
-          provider = options.tenant_provider.new(self)
-        else
-          provider = options  # if pass has to config, get mapped right on to ptions
-        end
-
-        options.client_id = provider.client_id
-        options.client_secret = provider.client_secret
-        options.tenant_id = 
-          provider.respond_to?(:tenant_id) ? provider.tenant_id : 'common'
-
-        options.authorize_params.domain_hint = provider.domain_hint if provider.respond_to?(:domain_hint) && provider.domain_hint
-        options.client_options.authorize_url = "#{BASE_AZURE_URL}/#{options.tenant_id}/oauth2/authorize"
-        options.client_options.token_url = "#{BASE_AZURE_URL}/#{options.tenant_id}/oauth2/token"
-
-        options.token_params.resource = options.resource
-        super
-      end
-
-      uid {
-        raw_info['sub']
-      }
+      uid { raw_info['id'] }
 
       info do
         {
-          name: raw_info['name'],
-          nickname: raw_info['unique_name'],
-          first_name: raw_info['given_name'],
-          last_name: raw_info['family_name'],
-          email: raw_info['email'] || raw_info['upn'],
-          oid: raw_info['oid'],
-          tid: raw_info['tid']
+          name: raw_info['displayName'],
+          first_name: raw_info['givenName'],
+          last_name: raw_info['surname'],
+          email: raw_info['email'] || raw_info['userPrincipalName']
         }
       end
 
-
       def raw_info
-        # it's all here in JWT http://msdn.microsoft.com/en-us/library/azure/dn195587.aspx
-        @raw_info ||= ::JWT.decode(access_token.token, nil, false).first
+        @raw_info ||= access_token.get('https://graph.microsoft.com/v1.0/me').parsed
       end
-
     end
   end
 end
